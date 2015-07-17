ec3f26a99cf703fb876f8431dc5d0b53af3e7dfe
Martin Llofriu <mllofriu@gmail.com>
2015-07-17 11:41:29 -0400

    no printing

--- src/edu/usf/experiment/utils/Debug.java
+++ src/edu/usf/experiment/utils/Debug.java
@@ -6,25 +6,28 @@ public class Debug {
 	public static boolean moveRobot = true;
 	public static boolean sleepBeforeStart = false;
 	
-	public static boolean print = false;
+	public static boolean print = true;
 	
-	public static boolean printValues = print && true;
+	public static boolean printValues = print && false;
 	public static boolean printEndCycle = print && false;
 	public static boolean printAnyGoal = print && false;
 	public static boolean printTaxic = print && false;
 	public static boolean printActiveGoal = print && false;
 	public static boolean printRobotAte = print && false;
-	public static boolean printIntention = print && true;
+	public static boolean printIntention = print && false;
 	public static boolean printTryingToEat = print && false;
 	public static boolean printFoundNNoMistakes = print && false;
 	public static boolean printHDCells = print && false;
 	public static boolean printGiveFood = print && false;
 	public static boolean printTaxicBh = print && false;
-	public static boolean printSelectedValues = print && true;	
+	public static boolean printSelectedValues = print && false;	
 	public static boolean printRobotEaten = print && false;
 	public static boolean printFeederCells = print && false;
 	public static boolean printJointMultiply = print && false;
 	public static boolean printExploration = print && false;
-	public static boolean printAttentional = print && false;	
+	public static boolean printAttentional = print && false;
+	public static boolean printConjCells = print && false;
+	public static boolean printHalfAndHalf = print && false;
+	public static boolean printExecutionOrder = print && false;	
 	
 }
