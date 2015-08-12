2481ff0cf1275cf8a5f5606e15b90bd2e98e91e6
Martin Llofriu <mllofriu@gmail.com>
2015-08-12 17:48:20 -0400

    Plot gatherer

---
+++ src/edu/usf/experiment/plot/PlotGatherer.java
@@ -0,0 +1,22 @@
+package edu.usf.experiment.plot;
+
+import java.net.URL;
+
+import edu.usf.experiment.utils.ElementWrapper;
+import edu.usf.experiment.utils.IOUtils;
+
+public class PlotGatherer extends Plotter {
+
+	public PlotGatherer(ElementWrapper params, String logPath) {
+		super(params, logPath);
+	}
+
+	@Override
+	public void plot() {
+		String logPath = getLogPath();
+		IOUtils.exec("mkdir plots", logPath);
+		IOUtils.exec("find . -iname '*.pdf' -exec ln {} plots/ \\;", logPath);
+		IOUtils.exec("find . -iname '*.png' -exec ln {} plots/ \\;", logPath);
+	}
+
+}
